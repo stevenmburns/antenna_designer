23a32167201be1caff1527a5740c089a6365105a
Steven Burns <smburns47@gmail.com>
2024-11-27 14:51:59 -0800

    Default to display

--- src/antenna_designer/cli.py
+++ src/antenna_designer/cli.py
@@ -11,37 +11,39 @@ def cli(arguments=None):
 
     subparsers = parser.add_subparsers(dest='command')
 
-    parser_draw = subparsers.add_parser('draw', help='Draw antenna')
-    parser_draw.add_argument('--fn', type=str, default=None, help='Plot goes to the file, or displayed on screen if None.')
-    parser_draw.add_argument('--builder', type=str, default='dipole', help='Use this antenna builder.')
+    def add_common(p, use_builders=False):
+        p.add_argument('--fn', type=str, default=None, help='Plot goes to the file, or displayed on screen if None.')
+        if use_builders:
+            p.add_argument('--builders', type=str, nargs='+', default=['dipole', 'invvee'], help='Use this list of antenna builders.')
+        else:
+            p.add_argument('--builder', type=str, default='dipole', help='Use this antenna builder.')
 
 
-    parser_sweep = subparsers.add_parser('sweep', help='Sweep antenna')
-    parser_sweep.add_argument('--fn', type=str, default=None, help='Plot goes to the file, or displayed on screen if None.')
-    parser_sweep.add_argument('--builder', type=str, default='dipole', help='Use this antenna builder.')
-    parser_sweep.add_argument('--param', type=str, default='freq', help='Use this sweep parameter.')
-    parser_sweep.add_argument('--range', nargs=2, default=None, type=float, help='Use this sweep range.')
-    parser_sweep.add_argument('--center', default=None, type=float, help='Use this to construct the sweep range.')
-    parser_sweep.add_argument('--fraction', default=None, type=float, help='Use this to construct the sweep range.')
-    parser_sweep.add_argument('--npoints', default=21, type=int, help='Use this as the number of points in the sweep.')
-    parser_sweep.add_argument('--gain', default=False, action='store_true', help='Plot gain instead of impedance.')
+    p = subparsers.add_parser('draw', help='Draw antenna')
+    add_common(p)
 
-    parser_optimize = subparsers.add_parser('optimize', help='Optimize antenna')
-    parser_optimize.add_argument('--fn', type=str, default=None, help='Plot goes to the file, or displayed on screen if None.')
-    parser_optimize.add_argument('--builder', type=str, default='dipole', help='Use this antenna builder.')
-    parser_optimize.add_argument('--params', nargs='+', default=None, type=str, help='Use these optimization params.')
-    parser_optimize.add_argument('--z0', default=50, type=float, help='Use this reference impedance.')
-    parser_optimize.add_argument('--resonance', default=False, action='store_true', help='Optimize to resonance instead of matching an impedance.')
-    parser_optimize.add_argument('--opt_gain', default=False, action='store_true', help='Also try to optimize gain.')
+    p = subparsers.add_parser('sweep', help='Sweep antenna')
+    add_common(p)
+    p.add_argument('--param', type=str, default='freq', help='Use this sweep parameter.')
+    p.add_argument('--range', nargs=2, default=None, type=float, help='Use this sweep range.')
+    p.add_argument('--center', default=None, type=float, help='Use this to construct the sweep range.')
+    p.add_argument('--fraction', default=None, type=float, help='Use this to construct the sweep range.')
+    p.add_argument('--npoints', default=21, type=int, help='Use this as the number of points in the sweep.')
+    p.add_argument('--gain', default=False, action='store_true', help='Plot gain instead of impedance.')
 
-    parser_pattern = subparsers.add_parser('pattern', help='Display far field of antenna')
-    parser_pattern.add_argument('--fn', type=str, default=None, help='Plot goes to the file, or displayed on screen if None.')
-    parser_pattern.add_argument('--builder', type=str, default='dipole', help='Use this antenna builder.')
-    parser_pattern.add_argument('--wireframe', default=False, action='store_true', help='Draw wireframe.')
+    p = subparsers.add_parser('optimize', help='Optimize antenna')
+    add_common(p)
+    p.add_argument('--params', nargs='+', default=None, type=str, help='Use these optimization params.')
+    p.add_argument('--z0', default=50, type=float, help='Use this reference impedance.')
+    p.add_argument('--resonance', default=False, action='store_true', help='Optimize to resonance instead of matching an impedance.')
+    p.add_argument('--opt_gain', default=False, action='store_true', help='Also try to optimize gain.')
+
+    p = subparsers.add_parser('pattern', help='Display far field of antenna')
+    add_common(p)
+    p.add_argument('--wireframe', default=False, action='store_true', help='Draw wireframe.')
 
     p = subparsers.add_parser('compare_patterns', help='Display far field of multiple antennas')
-    p.add_argument('--fn', type=str, default=None, help='Plot goes to the file, or displayed on screen if None.')
-    p.add_argument('--builders', type=str, nargs='+', default=['dipole', 'invvee'], help='Use this list of antenna builders.')
+    add_common(p, use_builders=True)
 
     args = parser.parse_args(args=arguments)
 
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,16 @@
 
 import antenna_designer as ant
-from antenna_designer.designs.dipole import Builder
 
-import sys
+o = ' --fn /dev/null'
+#o = ''
 
 def test_cli():
-  ant.cli('draw --fn /dev/null'.split())
-  ant.cli('draw --builder moxon --fn /dev/null'.split())
-  ant.cli('sweep --param tipspacer_factor --builder moxon --fn /dev/null'.split())
-  ant.cli('optimize --params length slope --builder invvee --fn /dev/null'.split())
-  ant.cli('optimize --params length slope --z0 60 --builder invvee --fn /dev/null'.split())
-  ant.cli('compare_patterns --fn /dev/null'.split())
+
+  ant.cli(f'draw{o}'.split())
+  ant.cli(f'draw --builder moxon{o}'.split())
+  ant.cli(f'sweep --param tipspacer_factor --builder moxon{o}'.split())
+  ant.cli(f'optimize --params length slope --builder invvee{o}'.split())
+  ant.cli(f'optimize --params length slope --z0 60 --builder invvee{o}'.split())
+  ant.cli(f'compare_patterns{o}'.split())
+  ant.cli(f'compare_patterns --builders invvee moxon{o}'.split())
+  ant.cli(f'compare_patterns --builders invvee hexbeam{o}'.split())
