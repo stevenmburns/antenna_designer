162372ae0f1a60c688d3ba78a101342f8c49bbcf
Steven Burns <smburns47@gmail.com>
2026-01-28 16:53:57 -0800

    Update slanted hentenna-like models

--- src/antenna_designer/designs/freq_based/hentenna_slant.py
+++ src/antenna_designer/designs/freq_based/hentenna_slant.py
@@ -1,3 +1,5 @@
+import math
+
 from ... import AntennaBuilder
 from ... import Transform, TransformStack
 
@@ -6,22 +8,14 @@ from types import MappingProxyType
 
 
 class Builder(AntennaBuilder):
-  params_100 = MappingProxyType({
-    'design_freq': 28.47,
-    'freq': 28.47,
-    'base': 10,
-    'top_height_factor': 0.4589,
-    'mid_height_factor': 0.0962,
-    'width_factor': 0.1841,
-  })
-
   params_50 = MappingProxyType({
     'design_freq': 28.47,
     'freq': 28.47,
     'base': 10,
-    'top_height_factor': 0.5081,
-    'mid_height_factor': 0.1094,
-    'width_factor': 0.1378,   
+    'top_height_factor': 0.4903,
+    'mid_height_factor': 0.1042,
+    'width_factor': 0.1577,   
+    'slant_degrees': 30,
   })
   
   default_params = params_50
@@ -30,9 +24,12 @@ class Builder(AntennaBuilder):
     eps = 0.05
     b = self.base
 
-
     wavelength = 299.792458/self.design_freq
  
+    slant_radians = self.slant_degrees/180*math.pi
+    cos_slant = math.cos(slant_radians)
+    sin_slant = math.sin(slant_radians)
+
     def ry(p):
       return  p[0], -p[1], p[2]
     def rz(p):
@@ -42,7 +39,9 @@ class Builder(AntennaBuilder):
     n_seg1 = 3
 
     """
- C----------------------------A
+    We will also model an invvee-like slant from the center for all three lines
+    Zero degrees is horizonal
+ C-------------AA-------------A
  |                            |
  |                            |
  |                            |
@@ -55,13 +54,16 @@ class Builder(AntennaBuilder):
  |                            |
  |                            |
  |                            |
- E----------------------------F
+ E-------------FF-------------F
     """
 
     S = (0, eps, wavelength*(self.mid_height_factor-self.top_height_factor))
-    B = (0, wavelength*self.width_factor/2, wavelength*(self.mid_height_factor-self.top_height_factor))
-    A = (0, wavelength*self.width_factor/2, 0)
-    F = (0, wavelength*self.width_factor/2, wavelength*(-self.top_height_factor))
+    B = (0, wavelength*self.width_factor/2*cos_slant, wavelength*(self.mid_height_factor-self.top_height_factor)-wavelength*self.width_factor/2*sin_slant)
+    A = (0, wavelength*self.width_factor/2*cos_slant, -wavelength*self.width_factor/2*sin_slant)
+    AA = (0, 0, 0)
+
+    F = (0, wavelength*self.width_factor/2*cos_slant, wavelength*(-self.top_height_factor)-wavelength*self.width_factor/2*sin_slant)
+    FF = (0, 0, wavelength*(-self.top_height_factor))
    
     C, D, T = ry(A), ry(B), ry(S)
     E = ry(F)
@@ -74,8 +76,8 @@ class Builder(AntennaBuilder):
 
     tups = []
 
-    tups.extend(build_path([B,A,C,D], n_seg0, None))
-    tups.extend(build_path([B,F,E,D], n_seg0, None))
+    tups.extend(build_path([B,A,AA,C,D], n_seg0, None))
+    tups.extend(build_path([B,F,FF,E,D], n_seg0, None))
     tups.extend(build_path([S,B], n_seg0, None))
     tups.extend(build_path([D,T], n_seg0, None))
     tups.extend(build_path([T,S], n_seg1, 1+0j))
